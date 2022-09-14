d27040b853dce90d2c2596270c888dceb9563cf7
couchmeka <meka.couch2@gmail.com>
2022-09-14 15:58:06 -0400

    saving filed

--- what-can-you-do.js
+++ what-can-you-do.js
@@ -2,4 +2,11 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let enterAge = Number(prompt("please enter your age: "));
 
+if (enterAge < 16){
+    console.log("You can't drive");
+} else if (enterAge === 16,17) 
+{
+    console.log("you can drive and not vote")
+}
+
 console.log(enterAge);
