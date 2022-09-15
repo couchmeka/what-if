1662336989ff6b9fcc0fcdcb41a4dad01ba8f5ae
couchmeka <meka.couch2@gmail.com>
2022-09-15 12:31:34 -0400

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,17 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let num1 =  Number(prompt("Pick a number: "));
 
-if (num1 % 3)
+
+ if (num1 % 15 ===0)
+{
+    console.log("fizzbuzz");
+} else if (num1 % 3 ===0)
 {
     console.log("fizz");
 
-} else if (num1 % 5)
+} else if (num1 % 5 ===0)
 {
     console.log("buzz");
-
-} else if (num1 % 5 == num1 % 3)
-{
-    console.log("fizzbuzz");
 } else
 {
     console.log("no fizz")
